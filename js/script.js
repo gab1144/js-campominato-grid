3b234ef7c486cacd2aecaa6b6d1b2337b8d41bda
gab1144 <gabry.rinciari04@gmail.com>
2022-10-20 15:10:36 +0200

    [add] function clickSquare e classe clicked

--- js/script.js
+++ js/script.js
@@ -4,16 +4,25 @@ document.getElementById('play').addEventListener('click', function(){
   init();
 })
 
-function init(numElements){
-  for (let i = 0; i < 100; i++){
-  createSquare(i);
+function init(){
+  container.innerHTML ='';
+  for (let i = 1; i <= 100; i++){
+    createSquare(i);
   }
+  container.style.border = "1px solid black";
+  container.style.background= "white";
 }
 
 function createSquare(id){
   const square = document.createElement('div');
   square.className = 'square';
   square.idElement = id;
-  square.innerText = id + 1;
+  square.innerText = id;
   container.append(square);
+  square.addEventListener('click', clickSquare)
+}
+
+function clickSquare(event){
+  console.log(this.idElement);
+  this.classList.add('clicked');
 }
